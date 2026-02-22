5aa5fbebfedf6c9584a1f34d2aed88067807dbbc
Derek Cormier <derek.m.cormier@gmail.com>
2026-02-22 15:04:27 -0800

    bazel9 and overlay ergonomics

--- overlay.sh
+++ overlay.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
+# Copy files over to a local bazel-central-registry.
+#
+# Args:
+#   <BCR> - Path to the registry root to copy files to
+
 set -o nounset -o errexit -o pipefail
 
-MODULE=~/Code/bazel-central-registry/modules/diffutils/3.12
+if [ -z "${1:-}" ]; then
+    echo "error: missing BCR root argument"
+    exit 1
+fi
+
+BCR="${1%/}"
+VERSION="3.12"
+INTEGRITY="sha256-fIt/n8hgkUH96pzs6FJJ0whiQ5H/Yd7a9Sj8szdyff0="
+MODULE="$BCR/modules/diffutils/${VERSION}"
 
 FILES=(
     BUILD.bazel
@@ -21,8 +34,17 @@ for file in "${FILES[@]}"; do
     cp "$file" "$MODULE/overlay/$file"
 done
 
-echo "\"overlay\": {"
-for file in "${FILES[@]}"; do
-    echo "  \"$file\": \"sha256-$(shasum -a 256 "$file" | awk '{print $1}' | xxd -r -p | base64)\","
-done
-echo "}"
+{
+    for file in "${FILES[@]}"; do
+        jq --null-input \
+            --compact-output \
+            --arg file "$file" \
+            --arg sha "sha256-$(shasum -a 256 "$file" | awk '{print $1}' | xxd -r -p | base64)" \
+            '{$file:$sha}'
+    done
+} | jq --slurp \
+    --arg url "https://mirror.cs.odu.edu/gnu/diffutils/diffutils-${VERSION}.tar.xz" \
+    --arg integrity "$INTEGRITY" \
+    --arg strip_prefix "diffutils-${VERSION}" \
+    --indent 4 \
+    '{url: $url, integrity: $integrity, strip_prefix: $strip_prefix, overlay: add}' > "$MODULE/source.json"
